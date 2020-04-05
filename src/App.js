bac3bed5ea21996432ce048f6591e8ee58541557
Fernando dos Santos <nando4803@gmail.com>
2020-04-05 16:29:46 +0100

    adicona os components no app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import { Cards, Chart, Country } from './components';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <Country />
+        <Chart />
       </div>
     )
   }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards } from './Cards';
+export { default as Chart } from './Chart';
+export { default as Country } from './Country';
